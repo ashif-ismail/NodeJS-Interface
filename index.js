5a8132e2a91864994ac044e00bf28d45c76e1c23
mukthar <technocults@gmail.com>
2015-05-27 16:04:09 +0530

    demo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var http = require('http');
+var request = require("request");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World,This Page Is Visible Only Through Your Browser!');
+  response.send('Hello World!');
 });
 
 var bodyParser = require('body-parser');
@@ -21,40 +21,19 @@ app.post('/telerivet/webhook',
           res.status(403).end();
           return;
       }
-    
-
-http.request(options, callback).end();
-var options = {
-  host: 'www.boilerpipe-web.appspot.com',
-  path: '/extract?url=http://www.caclub.in&extractor=ArticleExtractor&output=text&extractImages='
-};
-
-callback = function(response) {
-  var str = '';
-
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
-}
-
-http.request(options, callback).end();
-    if (req.body.event == 'incoming_message') {
+      
+      if (req.body.event == 'incoming_message') {
       
         var content = req.body.content;
         var from_number = req.body.from_number;
         var phone_id = req.body.phone_id;
-        
+        request("http://boilerpipe-web.appspot.com/extract?url=http%3A%2F%2Fwww.caclub.in&extractor=ArticleExtractor&output=text&extractImages=", function(error, response, body) {
+  console.log(body);
+});
         // do something with the message, e.g. send an autoreply
-	 
         res.json({
           messages: [
-            { content: "  " + str }
+            { content: " "+body }
           ]
         });
         
