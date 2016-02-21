b57c268cf96650377b079824810249a59d9aec79
SheikhZayed <cyber2maro@gmail.com>
2016-02-21 09:34:55 +0530

    new

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/telerivet/webhook',
       if (req.body.event == 'incoming_message') {
         var content = req.body.content;
         var from_number = req.body.from_number;
-	      var phone_id = req.body.phone_id;
+        var phone_id = req.body.phone_id;
 	  }
     	request("http://offlinebrowser-web.appspot.com/ExtractServlet?url=http://"+content, function(error, response, data) {
         // do something with the message, e.g. send an autoreply
@@ -28,20 +28,21 @@ app.post('/telerivet/webhook',
     //         { content:" " + data}
     //       ]
 		// });
-//     var len = data.length;
-//     var chunks = [];
-//     var chunkSize = 160;
-//     while (data) {
-//     if (len < chunkSize) {
-//         chunks.push(data);
-//         break;
-//     }
-//     else {
-//         chunks.push(data.substr(0, chunkSize));
-//         data = data.substr(chunkSize);
-//     }
-// }
-    request("http://ancient-lowlands-31895.herokuapp.com/?uid=9947753535&pwd=thepassword&to="+from_number+"&msg="+data, function(error, response, body) {
+    var len = data.length;
+    var chunks = [];
+    var chunkSize = 160;
+    while (data) {
+    if (len < chunkSize) {
+        chunks.push(data);
+        break;
+    }
+    else {
+        chunks.push(data.substr(0, chunkSize));
+        data = data.substr(chunkSize);
+    }
+}
+
+    request("http://ancient-lowlands-31895.herokuapp.com/?uid=9947753535&pwd=thepassword&to="+from_number+"&msg="+chunks, function(error, response, body) {
   });
        res.status(200).end();
 		 });
