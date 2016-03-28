f888e194c61e9410908fa4397b8f14082f4c4893
SheikhZayed <cyber2maro@gmail.com>
2016-03-28 20:41:39 +0530

    demo

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ var express = require('express');
 var request = require("request");
 var bodyParser = require('body-parser');
 var app = express();
+var userName = "abdulashif";
+var passWord = "sZd5y6AA";
+var senderID = "CDMLAB";
 var content;
 var from_number;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
-  response.send('We Dont Reply for GET/,make a POST/ instead..');
+  response.send('We Dont Reply for GET/,make a POST/ instead..!');
 });
 
 app.post('/',
   bodyParser.urlencoded({ extended: true }),
   function(req, res) {
-        content = req.body.content;
+        content = req.body.content;        // Parameters passed as POST Request from my Android App
         from_number = req.body.from_number;
         console.log('making request to offlinebrowser-web with URL as ' + content + ' and sender as ' + from_number );
         request("http://offlinebrowser-web.appspot.com/ExtractServlet?url=http://"+content+"&OutputType=1&ExtractorType=1", function(error, response, data) {
-        console.log('backend response : ' + data);
-        console.log('initiating response module');
-        request("http://ancient-lowlands-31895.herokuapp.com/sendsms.php?uid=9947753535&pwd=thepassword&phone=9947753535&msg=Hello+World", function(error, response, body) {
-            console.log(body);
+        console.log('backend response : ' + data); //logs to console the valid output of GET Request
+        console.log('initiating response module'); 
+        request("193.105.74.159/api/v3/sendsms/plain?user="+userName+"&password="+passWord+"sZd5y6AA&sender="+senderID+"&SMSText="+data+"&type=longsms&GSM="+from_number+", function(error, response, body)
+        //making a GET request to the above link in browser,perfectly sends an SMS,but here doesnt work!
+        {
+            console.log(body); //this line logs undefined to the console
         });
         res.status(200).end();
         });
