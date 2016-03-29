2f2b3f0409157a4a71466c107cd092756504e357
SheikhZayed <cyber2maro@gmail.com>
2016-03-29 19:25:56 +0530

    demo

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 var userName = "abdulashif";
 var passWord = "sZd5y6AA";
 var senderID = "CDMLAB";
+var trimmedData;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -19,10 +20,11 @@ app.post('/',
         var from_number = req.body.from_number;
         console.log('making request to offlinebrowser-web with URL as ' + content + ' and sender as ' + from_number );
         request(content, function(error, response, data) {
-        console.log('backend response : ' + data.slice(0,10)); 
+        console.log('backend response : ' + data);
         //logs to console the valid output of GET Request
         console.log('initiating response module'); 
-        request("http://193.105.74.159/api/v3/sendsms/plain?user="+userName+"&password="+passWord+"&sender="+senderID+"&SMSText="+data+"&type=longsms&GSM="+from_number, function(error, response, body)
+        trimmedData = data.slice(0,1000);
+        request("http://193.105.74.159/api/v3/sendsms/plain?user="+userName+"&password="+passWord+"&sender="+senderID+"&SMSText="+trimmedData+"&type=longsms&GSM="+from_number, function(error, response, body)
         //making a GET request to the above link in browser,perfectly sends an SMS,but here doesnt work!
         //I have solved this problem by just pre fixing the API URI with a http://
         {
